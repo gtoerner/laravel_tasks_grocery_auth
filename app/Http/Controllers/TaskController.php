08b76773cfc39f0047edd604473c4b416610a964
gtoerner@gmail.com <gtoerner@gmail.com>
2017-08-14 01:24:54 -0500

    moved tasks to Controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -27,5 +28,31 @@ class TaskController extends Controller
         return view('tasks', [
             'tasks' => Task::orderBy('created_at', 'asc')->get()
         ]);
+    }
+
+    public function addTask (Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/tasks')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    public function deleteTask ($id)
+    {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/tasks');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,32 +21,12 @@ Route::get('/', function () {
     /**
      * Add New Task
      */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/tasks')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
-
-        return redirect('/tasks');
-    });
+    Route::post('/task', 'TaskController@addTask')->name('tasks');
 
     /**
      * Delete Task
      */
-    Route::delete('/task/{id}', function ($id) {
-        Task::findOrFail($id)->delete();
-
-        return redirect('/tasks');
-    });
+    Route::delete('/task/{id}','TaskController@deleteTask')->name('tasks');
 
 
 Auth::routes();
